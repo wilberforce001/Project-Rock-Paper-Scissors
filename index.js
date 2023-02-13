45b325f5b5f64c02b17bb5265b88faae3df04737
wilberforce001 <kenmisik@gmail.com>
2023-02-13 20:32:39 +0300

    If else statements that declared the winner and the loser in the game

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ const getComputerChoice = () => {
     return compChoice
 
 }
-console.log(getComputerChoice())
+
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You tied! You both picked rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You tied! You both picked paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You tied! You both picked scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lost! Rock crushes scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return "You won! Scissors cuts paper"
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lost! Paper covers rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You won! Rock crushes scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lost! Scissors cuts paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You won! Paper covers rock'
+    }
+}
