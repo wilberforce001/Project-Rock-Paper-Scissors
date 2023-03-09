977c3aa4ff146b8c0b4ad4178b1ab27432575d0a
wilberforce001 <kenmisik@gmail.com>
2023-03-09 20:33:26 +0300

    Set functions that play a 5 round game that keeps score and reports a winner or loser at the end

--- rps-project/index.js
+++ rps-project/index.js
@@ -53,11 +53,28 @@ function getPlayerChoice() {
 }
 
 function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
     console.log("Welcome!")
     for (let i = 0; i < 5; i++) {
         const playerSelection = getPlayerChoice();
         const ComputerSelection = getComputerChoice();
         console.log(playRound(playerSelection, ComputerSelection));
+        console.log("------")
+        if(checkWinner(playerSelection, ComputerSelection) == "Player") {
+            scorePlayer++;
+        } else if(checkWinner(playerSelection, ComputerSelection) == "Computer"){
+            scoreComputer++;
+        }
      }
+     console.log("Game Over!")
+     if(scorePlayer > scoreComputer) {
+        console.log("player was the winner!")
+     }
+     else if(scorePlayer < scoreComputer){
+        console.log("Computer was the winner!")
+     } 
+     else
+     console.log("We have a tie!")
 }
-game();
+game()
