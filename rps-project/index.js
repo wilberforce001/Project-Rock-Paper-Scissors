9308da275cc72a05bc5947f44c538dd769835612
wilberforce001 <kenmisik@gmail.com>
2023-03-09 18:09:41 +0300

    Set the conditions that results in a tie or a win between the two players

--- rps-project/index.js
+++ rps-project/index.js
@@ -4,7 +4,24 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
-    console.log(choice);
+    return choice;
+    
+}
+function checkWinner(playerSelection, ComputerSelection) {
+    if (playerSelection == ComputerSelection) {
+        return"Tie!"
+    } else if(
+        (playerSelection == "rock" && ComputerSelection == "scissors")||
+        (playerSelection == "scissors" && ComputerSelection == "paper")||
+        (playerSelection == "paper" && ComputerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer"
+    }
+}
+function playRound(playerSelection, ComputerSelection){
     
 }
 
